977c86de886eb86d2c9efc7217bb3c14eb486d4e
sablier <albertpeng1991@gmail.com>
2019-05-16 12:00:52 +0800

    remove remote file

--- remote_meau.py
+++
@@ -1,3 +0,0 @@
-1. spider_neir.py   get all kinds of descriminations of wallpapper of neir 
-2. spider_neir.py   get highest descrimination of wallpapper of neir, also using the RE.
-3. spider_kion.py   get highest descrimination of wallpapper of kion grils, it is modified from neir_RE, just need to change path and url of main.
